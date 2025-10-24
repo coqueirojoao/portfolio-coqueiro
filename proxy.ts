c8170ce4d995fe7f3f052ceb55378c4d3dfedfb2
João Pedro <azevedo.joao1995@gmail.com>
2025-10-24 15:11:01 -0300

    fix: exclude cv folder from proxy matcher to allow resume downloads

--- proxy.ts
+++ proxy.ts
@@ -8,6 +8,6 @@ export default createMiddleware({
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|cv).*)",
   ],
 };
